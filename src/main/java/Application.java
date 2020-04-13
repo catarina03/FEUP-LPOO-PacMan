717025731bdac350f0ebd493687f7cbb87692969
Ca-moes <andre3000pt@gmail.com>
2020-04-13 19:40:46 +0100

    Part 1 and 3 Done. Larger Map

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,13 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TimeZone;
 
 public class Application {
     public static void main(String[] args) {
---
+++ src/main/java/ReadFile.java
@@ -0,0 +1,28 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.ArrayList;
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class ReadFile {
+    private File myObj;
+    private Scanner myReader;
+    ReadFile(){
+        try {
+            myObj = new File("mapv2.txt");
+            myReader = new Scanner(myObj);
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> fileContent(){
+        ArrayList<String> arrayList= new ArrayList<>();
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            arrayList.add(data);
+        }
+        myReader.close();
+        return arrayList;
+    }
+}
