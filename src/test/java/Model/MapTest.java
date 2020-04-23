6904ae0a84aafba91c8e289cf9f6218a175572a9
catarina03 <up201806610@fe.up.pt>
2020-04-23 19:04:41 +0100

    Starting MapTests

---
+++ src/test/java/Model/MapTest.java
@@ -0,0 +1,27 @@
+package Model;
+
+import Model.Elements.Ghost;
+import Model.Elements.PacMan;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class MapTest {
+
+    @Test
+    public void MapManipulation() {
+        Map map = new Map();
+
+        Ghost ghost1 = Mockito.mock(Ghost.class);
+
+
+        assertEquals(data.getGhosts(), ghosts);
+        assertEquals(data.getPacMan(), pacman);
+        assertEquals(data.getGameStats(), stats);
+        assertEquals(data.getMap(), map);
+    }
+
+}
