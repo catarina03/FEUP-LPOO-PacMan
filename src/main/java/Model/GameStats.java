21a93ddb48e96adba6e274ce1815a530b2349dbc
catarina03 <up201806610@fe.up.pt>
2020-04-23 17:44:28 +0100

    GameStats tests done

--- src/main/java/Model/GameStats.java
+++ src/main/java/Model/GameStats.java
@@ -19,19 +19,4 @@ public class GameStats {
         this.score = score;
     }
 
-    public void draw(TextGraphics textGraphics){
-        textGraphics.setForegroundColor(TextColor.ANSI.RED);
-        textGraphics.setBackgroundColor(TextColor.ANSI.BLACK);
-        textGraphics.putString(9, 1, "SCORE", SGR.BOLD);
-        textGraphics.putString(29, 1, "HI-SCORE", SGR.BOLD);
-        textGraphics.setForegroundColor(TextColor.ANSI.WHITE);
-        textGraphics.putString(13, 2, String.valueOf(score), SGR.BOLD);
-        textGraphics.putString(31, 2, "10000", SGR.BOLD);
-        textGraphics.setForegroundColor(TextColor.ANSI.YELLOW);
-        textGraphics.putString(9, 34, "00000", SGR.BOLD);
-        textGraphics.setForegroundColor(TextColor.ANSI.RED);
-        textGraphics.putString(35, 34, "o", SGR.BOLD);
-    }
-
-
 }
