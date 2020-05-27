994b1039fcd0397ac8f2f6026151583e775650e0
Ca-moes <andre3000pt@gmail.com>
2020-05-27 20:50:32 +0100

    Preencher métodos abstratos de GuiRectangle

--- src/main/java/g11/view/GuiRectangle.java
+++ src/main/java/g11/view/GuiRectangle.java
@@ -1,6 +1,9 @@
 package g11.view;
 
+import com.googlecode.lanterna.SGR;
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
@@ -45,26 +48,56 @@ public class GuiRectangle extends Gui {
 
     @Override
     void readyScreen() throws IOException {
+        getScreen().clear();
+        TextGraphics graphics = getScreen().newTextGraphics();
+        graphics.setBackgroundColor(TextColor.ANSI.BLACK);
 
+        graphics.setForegroundColor(TextColor.ANSI.WHITE);
+        graphics.putString(3, 0, "READY SCREEN", SGR.BOLD);
+        getScreen().refresh();
     }
 
     @Override
     void presentationScreen() throws IOException {
+        getScreen().clear();
+        TextGraphics graphics = getScreen().newTextGraphics();
+        graphics.setBackgroundColor(TextColor.ANSI.BLACK);
 
+        graphics.setForegroundColor(TextColor.ANSI.WHITE);
+        graphics.putString(3, 0, "PRESENTATION SCREEN", SGR.BOLD);
+        getScreen().refresh();
     }
 
     @Override
     void inicialScreen() throws IOException {
+        getScreen().clear();
+        TextGraphics graphics = getScreen().newTextGraphics();
+        graphics.setBackgroundColor(TextColor.ANSI.BLACK);
 
+        graphics.setForegroundColor(TextColor.ANSI.WHITE);
+        graphics.putString(3, 0, "INITIAL SCREEN", SGR.BOLD);
+        getScreen().refresh();
     }
 
     @Override
     void endScreen(Boolean winner, GameStats gameStats) throws IOException {
+        getScreen().clear();
+        TextGraphics graphics = getScreen().newTextGraphics();
+        graphics.setBackgroundColor(TextColor.ANSI.BLACK);
 
+        graphics.setForegroundColor(TextColor.ANSI.WHITE);
+        graphics.putString(3, 0, "END SCREEN", SGR.BOLD);
+        getScreen().refresh();
     }
 
     @Override
     void pauseScreen(int i) throws IOException {
+        getScreen().clear();
+        TextGraphics graphics = getScreen().newTextGraphics();
+        graphics.setBackgroundColor(TextColor.ANSI.BLACK);
 
+        graphics.setForegroundColor(TextColor.ANSI.WHITE);
+        graphics.putString(3, 0, "PAUSE SCREEN", SGR.BOLD);
+        getScreen().refresh();
     }
 }
